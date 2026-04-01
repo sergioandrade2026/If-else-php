3e47c8052c81988f9a864c1702580b86d6a2f1b4
sergioandrade2026 <sergioandrade2045@gmail.com>
2026-03-31 21:34:50 -0300

    Update CalculadoraIRPF.php

--- CalculadoraIRPF.php
+++ CalculadoraIRPF.php
@@ -29,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 
 if(isset($_POST["salario"])){
 
-$salario = $_POST["salario"];
+$salario = filter_input(INPUT_POST,$_POST["salario"],FILTER_VALIDATE_FLOAT);
 
 }
  
